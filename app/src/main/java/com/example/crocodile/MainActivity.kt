968eb772c75ec3af6c323605ccc292f6e720f761
poveshenko <e.poveshenko@gmail.com>
2024-01-30 19:29:17 +0300

    Изменение в проекте. 1. Добавил кнопку включения таймера 2. Так же функцию, для ее реализации

--- app/src/main/java/com/example/crocodile/MainActivity.kt
+++ app/src/main/java/com/example/crocodile/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.View
 import androidx.core.content.ContextCompat.startActivity
 import androidx.core.view.GravityCompat
@@ -11,15 +12,27 @@ import com.example.crocodile.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        //Музыкальное сопровождение
         val mediaPlayer = MediaPlayer.create(this, R.raw.click2)
         val mediaPlayer2 = MediaPlayer.create(this, R.raw.music)
 
+        //Подключение binding класса
         binding.apply {
 
+            //Подключение кнопки таймера
+            buttonStartTimer.setOnClickListener {
+                playTimer()
+
+                mediaPlayer.start()
+            }
+
+// Подключение кнопки для включение и выключения музыки
             buttonMusic.setOnClickListener {
 
                 if (mediaPlayer2.isPlaying)
@@ -28,13 +41,14 @@ class MainActivity : AppCompatActivity() {
                 else
 
                     mediaPlayer2.start();
-
             }
 
+//Подключение кнопки - "настройки"
             buttonSetting.setOnClickListener {
                 drawer.openDrawer(GravityCompat.START)
             }
 
+//Подключение меню навигации(Боковое меню)
             NavigationView.setNavigationItemSelectedListener {
 
                 when (it.itemId) {
@@ -42,26 +56,27 @@ class MainActivity : AppCompatActivity() {
                         buttonPlay.setOnClickListener {
                             textPlay.text = getMovie()
                             mediaPlayer.start()
-
                         }
-                       
                     }
 
                     R.id.carton -> {
                         buttonPlay.setOnClickListener {
                             textPlay.text = getCartoon()
+                            mediaPlayer.start()
                         }
                     }
 
                     R.id.work -> {
                         buttonPlay.setOnClickListener {
                             textPlay.text = getWork()
+                            mediaPlayer.start()
                         }
                     }
 
                     R.id.items -> {
                         buttonPlay.setOnClickListener {
                             textPlay.text = getItems()
+                            mediaPlayer.start()
                         }
                     }
 
@@ -71,6 +86,8 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    //Основные функции, бокового меню
 
     private fun getItems(): String {
         return resources.getStringArray(R.array.item)[randomNumberItem()]
@@ -109,8 +126,22 @@ class MainActivity : AppCompatActivity() {
         return (0..size).random()
     }
 
+    // функция таймер
+    private fun playTimer() {
+        object : CountDownTimer(30000, 1000) {
 
+            // Callback function, fired on regular interval
+            override fun onTick(millisUntilFinished: Long) {
+                binding.timer.text = "time: " + millisUntilFinished / 1000
+            }
 
+            // Callback function, fired
+            // when the time is up
+            override fun onFinish() {
+                binding.timer.text = ("done!")
+            }
+        }.start()
+    }
 
 
 }
